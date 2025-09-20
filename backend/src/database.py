b1dee99157f76eabab2c143c45d241c60f9fdf75
Roy <roy@gemini.com>
2025-09-20 00:04:38 -0500

    fix: Read SQLALCHEMY_DATABASE_URL from environment variable

--- backend/src/database.py
+++ backend/src/database.py
@@ -1,8 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
-SQLALCHEMY_DATABASE_URL = "postgresql://user:password@db/mydatabase"
+SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL", "postgresql://user:password@db/mydatabase")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
